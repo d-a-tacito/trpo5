3f08b8cf37988d20966f8bda56926467a1a3bbb1
dantonov <dananto02@gmail.com>
2024-11-21 20:51:05 +0300

    Added project trpo5

---
+++ trpo5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+
+namespace trpo5
+{
+    public class Program
+    {
+        public static string _newTaskTitle;
+
+        public static ObservableCollection<SimpleTask> Tasks = new();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите название задачи (введите end для завершения):");
+                _newTaskTitle = Console.ReadLine();
+
+                if (_newTaskTitle == "end")
+                    return;
+                
+                AddTask();
+
+                Console.WriteLine();
+                foreach (var simpleTask in Tasks)
+                {
+                    Console.WriteLine($"{simpleTask.Title}. Статус выполнения задачи - {simpleTask.IsCompleted}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(_newTaskTitle))
+            {
+                AddTaskInternal(_newTaskTitle);
+                _newTaskTitle = string.Empty;
+            }
+        }
+
+        private static void AddTaskInternal(string taskTitle)
+        {
+            Tasks.Add(new SimpleTask { Title = taskTitle });
+        }
+
+    }
+}
---
+++ trpo5/SimpleTask.cs
@@ -0,0 +1,8 @@
+﻿namespace trpo5;
+public class SimpleTask
+{
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public override string ToString() => Title;
+}
